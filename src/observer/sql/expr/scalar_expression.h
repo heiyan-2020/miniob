98be61c5b65bd7e11007c2ddc71763fd0993e3b0
张岳轩 <201250179@smail.nju.edu.cn>
2022-10-18 16:20:29 +0800

    fix(scalar_subquery): return fail when subquery return EOF

--- src/observer/sql/expr/scalar_expression.h
+++ src/observer/sql/expr/scalar_expression.h
@@ -13,35 +13,36 @@ public:
   {
     // By this time, subquery must be planned and prepared.
     assert(subquery_plan_);
-      SchemaRef subquery_schema = subquery_plan_->get_schema();
-      if (subquery_schema->get_column_count() != 1) {
-        LOG_PANIC("Subquery schema doesn't match");
-        return RC::MISMATCH;
-      }
-      RC rc;
-      TupleRef subquery_tuple;
-      subquery_plan_->initialize();
-      if ((rc = subquery_plan_->next()) == RC::SUCCESS) {
-        rc = subquery_plan_->current_tuple(subquery_tuple);
-        if (rc != RC::SUCCESS) {
-          LOG_PANIC("In operator: Getting tuple failed");
-          return rc;
-        }
-        out_value = subquery_tuple->get_value(subquery_schema, 0);
-      }
-
-      if (rc != RC::RECORD_EOF && rc != RC::SUCCESS) {
-        // terminate abnormally.
+    SchemaRef subquery_schema = subquery_plan_->get_schema();
+    if (subquery_schema->get_column_count() != 1) {
+      LOG_PANIC("Subquery schema doesn't match");
+      return RC::MISMATCH;
+    }
+    RC rc;
+    TupleRef subquery_tuple;
+    subquery_plan_->initialize();
+    if ((rc = subquery_plan_->next()) == RC::SUCCESS) {
+      rc = subquery_plan_->current_tuple(subquery_tuple);
+      if (rc != RC::SUCCESS) {
+        LOG_PANIC("In operator: Getting tuple failed");
         return rc;
       }
+      out_value = subquery_tuple->get_value(subquery_schema, 0);
+    }
 
-      rc = subquery_plan_->next();
+    if (rc != RC::SUCCESS) {
+      // terminate abnormally.
+      // if rc == RC::EOF, still return false because empty tuple isn't scalar value.
+      return RC::EVALUATE;
+    }
 
-      if (rc != RC::RECORD_EOF) {
-        // terminate abnormally.
-        return RC::SCALAR;
-      }
-      return RC::SUCCESS;
+    rc = subquery_plan_->next();
+
+    if (rc != RC::RECORD_EOF) {
+      // terminate abnormally.
+      return RC::SCALAR;
+    }
+    return RC::SUCCESS;
   }
 
   auto convert_to_column(SchemaRef schema, Column &out_col) -> RC override
